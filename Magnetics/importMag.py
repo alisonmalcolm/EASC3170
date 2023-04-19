81521efd8bb5552ddbc23543de0b5aef8689ef47
alisonmalcolm <amalcolm@mun.ca>
2023-04-19 12:56:09 -0230

    updated importMag to fix a widgets problem

--- Magnetics/importMag.py
+++ Magnetics/importMag.py
@@ -7,7 +7,9 @@ import subprocess
 import warnings
 warnings.filterwarnings('ignore') # ignore warnings: only use this once you are sure things are working
 
-from IPython.html.widgets import *
+#from IPython.html.widgets import *
+#from IPython.html.widgets import *
+import ipywidgets as widgets
 from IPython.display import display
 from Library import *
 from scipy.constants import mu_0
